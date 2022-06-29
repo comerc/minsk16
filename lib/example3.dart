362fca1368e823975a50f6baacdf359d99c99a05
comerc <andrew.kachanov@gmail.com>
2022-06-29 14:49:13 +0300

    Try BoardViewExample

---
+++ lib/example3.dart
@@ -0,0 +1,55 @@
+import 'package:boardview/boardview.dart';
+import 'package:boardview/boardview_controller.dart';
+import 'package:flutter/material.dart';
+
+class BoardViewExample extends StatefulWidget {
+  @override
+  _BoardViewExampleState createState() => _BoardViewExampleState();
+}
+
+class MyPage implements BoardPage {
+  final List<Widget> _widgets;
+
+  @override
+  List<Widget> get widgets => _widgets;
+
+  @override
+  double scrollPosition = 0;
+
+  @override
+  int id;
+
+  MyPage(this.id, this.scrollPosition, this._widgets) : name = "Page";
+
+  @override
+  String name;
+}
+
+class _BoardViewExampleState extends State<BoardViewExample> {
+  final List<BoardPage> _listData = [
+    MyPage(0, 0, [Text("Page 1 - Item 1"), Text("Page 1 - Item 2")]),
+    MyPage(1, 0, [Text("Page 2 - Item 1")]),
+  ];
+
+  BoardViewController boardViewController = new BoardViewController();
+
+  @override
+  Widget build(BuildContext context) {
+    return BoardView(
+      canDrag: true,
+      controller: boardViewController,
+      lists: _listData,
+      onItemDropped: (int oldListIndex, int newListIndex, int oldItemIndex,
+          int newItemIndex) {},
+      activeDotColor: null,
+      onLockPressed: (int listIndex) {},
+      onAttemptDelete: (int listIndex) {
+        return null;
+      },
+      onListDropped: (int oldListIndex, int newListIndex) {},
+      showPageIndicator: true,
+      onPageChanged: (int value) {},
+      onPageTitleChanged: (int pageIndex, String value) {},
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'example3.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  const MyCustomScrollBehavior() : super();
+
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        // PointerDeviceKind.stylus,
+        // PointerDeviceKind.unknown,
+      };
 }
 
 class MyApp extends StatelessWidget {
@@ -25,6 +40,15 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      scrollBehavior: MyCustomScrollBehavior(),
+      // scrollBehavior: MaterialScrollBehavior().copyWith(
+      //   dragDevices: {
+      //     PointerDeviceKind.mouse,
+      //     PointerDeviceKind.touch,
+      //     PointerDeviceKind.stylus,
+      //     PointerDeviceKind.unknown
+      //   },
+      // ),
     );
   }
 }
@@ -75,41 +99,42 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: BoardViewExample(),
+      // Center(
+      //   // Center is a layout widget. It takes a single child and positions it
+      //   // in the middle of the parent.
+      //   child: Column(
+      //     // Column is also a layout widget. It takes a list of children and
+      //     // arranges them vertically. By default, it sizes itself to fit its
+      //     // children horizontally, and tries to be as tall as its parent.
+      //     //
+      //     // Invoke "debug painting" (press "p" in the console, choose the
+      //     // "Toggle Debug Paint" action from the Flutter Inspector in Android
+      //     // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+      //     // to see the wireframe for each widget.
+      //     //
+      //     // Column has various properties to control how it sizes itself and
+      //     // how it positions its children. Here we use mainAxisAlignment to
+      //     // center the children vertically; the main axis here is the vertical
+      //     // axis because Columns are vertical (the cross axis would be
+      //     // horizontal).
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       const Text(
+      //         'You have pushed the button this many times:',
+      //       ),
+      //       Text(
+      //         '$_counter',
+      //         style: Theme.of(context).textTheme.headline4,
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
